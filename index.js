4f9d40e185d872c9b48f95e8305e8184ca1bd61d
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-01-20 09:50:26 -0800

    commit tree 75cc1f4efd81d150a3d8be9d05d15b3b0cf6d968

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = "*") {
+  return function (param = "special") {
+    return `You are ${flair}${param}${flair}!`;
+  };
+}
+
+let Calculator = {};
+
+Calculator.add = function (num1, num2) {
+  return num1 + num2;
+};
+
+Calculator.subtract = function (num1, num2) {
+  return num1 - num2;
+};
+
+Calculator.multiply = function (num1, num2) {
+  return num1 * num2;
+};
+
+Calculator.divide = function (num1, num2) {
+  return num1 / num2;
+};
+
+function actionApplyer(start, arr) {
+  if (arr.length === 0) {
+    return start;
+  } else {
+    arr.forEach((fun) => {
+      start = fun(start);
+    });
+    return start;
+  }
+}
